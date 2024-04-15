a367a7510f3be3493dbc1a6ac0ef59406a7207c1
Denis <denisbormusov@gmail.com>
2024-04-15 16:59:27 +0500

    add js file

---
+++ js/script.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const nameProduct = 'Телевизор';
+let quantityProduct = 5;
+const categoryProduct = 'Техника для дома';
+let priceProduct = 150;
+
+console.log(`Общая сумма товара: ${priceProduct * quantityProduct}$` );
