74a88692a03c9944d6a7c8ccdb6e636d20e1a4cb
Denis <denisbormusov@gmail.com>
2024-08-23 17:32:22 +0500

    Создать переменные для модального окна

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
-'use strict'
+'use strict';
 
+const modalTitle = document.querySelector('.jsModalTitle');
+const closeModal = document.querySelector('.jsModalClose');
+const modalProductId = document.querySelector('.jsIdProduct');
+const formAddProduct = document.querySelector('.jsFormAddProduct');
+const modalCheckbox = document.querySelector('.jsModalCheckbox');
+const modalDscountText = document.querySelector('.jsDiscountText');
+const modalTotalCost = document.querySelector('.jsModalTotalCost');
