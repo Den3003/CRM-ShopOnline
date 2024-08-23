c449189b8fd3ce763c48a8d4b5f8891f0c163c24
Denis <denisbormusov@gmail.com>
2024-08-23 14:29:59 +0500

    init CRM

--- js/script.js
+++ js/script.js
@@ -1,8 +1,2 @@
 'use strict'
 
-const nameProduct = 'Телевизор';
-const quantityProduct = 5;
-const categoryProduct = 'Техника для дома';
-const priceProduct = 150;
-
-console.log(`Общая сумма товара: ${priceProduct * quantityProduct}$` );
