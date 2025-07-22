fa974bc19c332ce24eefe3d9eec6e645b74f96ca
Denis <denisbormusov@gmail.com>
2025-07-22 18:48:31 +0500

    fixed API

--- js/modules/variables.js
+++ js/modules/variables.js
@@ -1,4 +1,4 @@
-export const URL = 'https://quiver-lucky-sting.glitch.me';
+export const URL = 'https://3aa4815d-4ee1-4789-99ce-c7187262f6f5-00-1cfe07zl84xve.janeway.replit.dev';
 export const PRODUCTS_LIST = '/api/goods';
 export const PRODUCTS_PAGE = '?page=';
 export const PRODUCTS_TOTAL_PRICE = '/api/total';
